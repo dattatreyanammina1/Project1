27b8a34d375f56e78f65234de484fa422fef309a
Nammina Dattatreya <147964790+dattatreyanammina1@users.noreply.github.com>
2024-07-01 13:15:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,3 +42,9 @@ function toggleDropdown(dropdownId, symbolId) {
     dropdownContent.classList.toggle('show');
     dropdownSymbol.classList.toggle('rotate');
 }
+
+var mainDiv = document.getElementById('main-button');
+mainDiv.addEventListener('click', function(){
+  this.children.item(0).classList.toggle('fa-times');
+  this.classList.toggle('open');
+});
